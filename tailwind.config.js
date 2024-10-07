1032e7e6af916229ac150c5742370e418993cd79
Ariel Hurtado <arielhurtado388@gmail.com>
2024-10-07 14:22:40 -0500

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: "'Lato', sans-serif",
+      },
+      colors: {
+        "green-color": "#27AE60",
+        "gray-color": "#828282",
+        "card-color": "#212121",
+        "bg-color": "#171717",
+        "menu-color": "#172554",
+      },
+      backgroundImage: {
+        "open-menu": "url('../assets/menu.svg')",
+        "close-menu": "url('../assets/close.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
