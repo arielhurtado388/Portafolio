69ac050b51389ffa8dc208cf5302f45aeaab64fe
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-12 14:07:03 -0500

    Nuevo proyecto

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -3,7 +3,7 @@ import type { Proyecto } from "../types";
 export const proyectos: Proyecto[] = [
   {
     id: 1,
-    url: "https://tandatask.arielhurtado.online/",
+    url: "https://tandatask.netlify.app/",
     titulo: "TandaTask",
     descripcion:
       "Es una aplicación implementada con Mongo, Express, React y Node (MERN) utilizando TypeScript, Tailwind, ZOD, React Router DOM, Axios, JWT, Mongoose y ReactQuery. Permite gestionar proyectos, tareas, notas y colaboradores al más puro estilo de trello.",
@@ -13,27 +13,27 @@ export const proyectos: Proyecto[] = [
     url: "https://questify.domcloud.dev/",
     titulo: "Questify",
     descripcion:
-      "Es una aplicación web que gestiona la información de las actividades turísticas de la parroquia de San Andrés en Riobamba - Ecuador. Este proyecto fue realizado para el GAD parroquial de San Andrés. Realizada con PHP, MySQL, SASS, JS, Creación propia del Router, ORM Básico y un mini Framework usando Active Record, POO y PDO.",
+      "Es una aplicación web que gestiona la información de las actividades turísticas de la parroquia de San Andrés en Riobamba - Ecuador. Realizada con PHP, MySQL, SASS, JS, Creación propia del Router, ORM Básico y un mini Framework usando Active Record, POO y PDO.",
   },
   {
     id: 3,
-    url: "https://bebidasec.netlify.app/",
-    titulo: "Bebidas",
+    url: "https://devtreeec.netlify.app/",
+    titulo: "DevTree",
     descripcion:
-      " Es una aplicación implementada con React, TypeScript, Tailwind, ZOD, Zustand, React Router DOM, Axios, Slice Pattern y OpenRouter que permite consultar recetas de bebidas y guardarlas como favoritos. Además, permite la generación de recetas con inteligencia artificial utlizando el modelo de Meta:Llama-3.3-70b.",
+      "Es una aplicación implementada con Mongo, Express, React y Node (MERN) utilizando TypeScript, Tailwind, React Router DOM, Axios, JWT, Mongoose y ReactQuery. Permite tener todos lo enlaces a nuestras redes sociales en un solo lugar, similar a como lo realiza LinkTree.",
   },
   {
     id: 4,
+    url: "https://bebidasec.netlify.app/",
+    titulo: "Bebidas",
+    descripcion:
+      "Es una aplicación implementada con React, TypeScript, Tailwind, ZOD, Zustand, React Router DOM, Axios, Slice Pattern y OpenRouter que permite consultar recetas de bebidas y guardarlas como favoritos. Además, permite la generación de recetas con inteligencia artificial utlizando el modelo de Meta:Llama-3.3-70b.",
+  },
+  {
+    id: 5,
     url: "https://bienesraices.domcloud.dev/",
     titulo: "BienesRaíces",
     descripcion:
       "Es una aplicación que permite publicar y administrar propiedades en venta, realizada con Node.js, Express, MySQL, PUG, JWT, Leaflet y Tailwind. Permite ver cada propiedad y enviar un mensaje al vendedor, filtrar propiedades por su categoría y precio o realizar una búsqueda generalizada. Cuenta con autenticación, panel de administración de propiedades, paginación, etc",
   },
-  // {
-  //   id: 5,
-  //   url: "https://rentapartament.netlify.app/",
-  //   titulo: "RentApartament",
-  //   descripcion:
-  //     "Es la maquetación de un sitio de arriendos para estudiantes universitarios. Este proyecto fue presentado en una casa abierta a los estudiantes de la Escuela Superior Politécnica de Chimborazo (ESPOCH).",
-  // },
 ];
